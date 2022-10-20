a99b3d3299d542db6c8a713b3331d3d034f943ab
Yrahcaz7 <zachary.shive@gmail.com>
2022-10-20 16:23:42 -0400

    add special exponentiation

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener("input", () => {
 	var output = "" + document.getElementById("input").value;
-	output = output.replace(/[^()\[\]0-9+\-*/%^]/g, ""); // delete illegal chars
-	output = output.replace(/([+\-*/%^]){2,}/g, "$1"); // delete redundant chars
+	output = output.replace(/[^()\[\]0-9+\-*/%^,]/g, ""); // delete illegal chars
+	output = output.replace(/([+\-*/%^,]){2,}/g, "$1"); // delete redundant chars
 	output = output.replace(/\(|\[/g, "{").replace(/\)|\]/g, "}"); // convert parentheses
 	output = output.replace(/\d{1,}/g, "&#8862"); // convert numbers
 	output = output.replace(/\+/g, "&#45"); // convert addition
@@ -10,7 +10,9 @@ document.addEventListener("input", () => {
 	output = output.replace(/(&#8862|\})(?=&#8862|\{)/g, "$1&#45O&#45"); // convert parenthesis multiplication
 	output = output.replace(/\/|%/g, "_|&#8254"); // convert division
 	output = output.replace(/\^&#8862/g, "&#45O&#45&#8853"); // convert exponentiation
-	output = output.replace(/\^/g, ""); // delete failed exponentiation
+	output = output.replace(/\^{&#8862,&#8862}/g, "&#45O&#45{&#8853 &#8862}"); // convert special exponentiation
+	output = output.replace(/\^\{/g, "&#45O&#45{").replace(/\^/g, ""); // delete failed exponentiation
 	output = output.replace(/-/g, "["); // convert negatives
+	output = output.replace(/,/g, ""); // delete extra commas
 	document.getElementById("output").innerHTML = output;
 });
