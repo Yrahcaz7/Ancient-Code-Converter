9e065d32e9ce33b9e8b8ff535f929c56840a3775
Yrahcaz7 <zachary.shive@gmail.com>
2022-10-20 18:22:25 -0400

    add comparators

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 document.addEventListener("input", () => {
 	var output = "" + document.getElementById("input").value;
-	output = output.replace(/[^()\[\]0-9+\-*/%^,]/g, ""); // delete illegal chars
-	output = output.replace(/([+\-*/%^,]){2,}/g, "$1"); // delete redundant chars
+	output = output.replace(/[^()\[\]0-9+\-*/%^,=!><]/g, ""); // delete illegal chars
+	output = output.replace(/(>|<|=)-/g, "$1 -"); // separate negatives and equalities
+	output = output.replace(/(\+|-|\*|\/|%|\^|,|>=|>|<=|<|!=){2,}/g, "$1"); // delete redundant chars
 	output = output.replace(/\(|\[/g, "{").replace(/\)|\]/g, "}"); // convert parentheses
 	output = output.replace(/\d{1,}/g, "&#8862"); // convert numbers
 	output = output.replace(/\+/g, "&#45"); // convert addition
@@ -13,6 +14,9 @@ document.addEventListener("input", () => {
 	output = output.replace(/\^{&#8862,&#8862}/g, "&#45O&#45{&#8853 &#8862}"); // convert special exponentiation
 	output = output.replace(/\^\{/g, "&#45O&#45{").replace(/\^/g, ""); // delete failed exponentiation
 	output = output.replace(/-/g, "["); // convert negatives
-	output = output.replace(/,/g, ""); // delete extra commas
+	output = output.replace(/>=/g, "]&#135&#135").replace(/>/g, "]&#12456"); // convert greater than
+	output = output.replace(/<=/g, "][&#135&#135").replace(/</g, "][&#12456"); // convert less than
+	output = output.replace(/!=/g, "[||").replace(/=/g, "||"); // convert equals
+	output = output.replace(/[,=!><\s]/g, ""); // delete extra chars
 	document.getElementById("output").innerHTML = output;
 });
